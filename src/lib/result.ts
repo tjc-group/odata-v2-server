af2be643f98dd524e490aecc365df1d91a8fad6b
Sergey Korolev <skorolev@tjc-group.com>
2020-05-29 11:34:13 +0100

    Fix inlinecount = 0

--- src/lib/result.ts
+++ src/lib/result.ts
@@ -1,26 +1,26 @@
 import { Readable, Writable } from "stream";
 
-export class ODataStream{
-    stream:any
-    contentType:string
+export class ODataStream {
+    stream: any
+    contentType: string
 
-    constructor(contentType:string);
-    constructor(stream:any, contentType?:string);
-    constructor(stream:any, contentType?:string){
-        if (typeof stream == "string"){
+    constructor(contentType: string);
+    constructor(stream: any, contentType?: string);
+    constructor(stream: any, contentType?: string) {
+        if (typeof stream == "string") {
             this.stream = null;
             this.contentType = stream;
-        }else{
+        } else {
             this.stream = stream;
             this.contentType = contentType;
         }
         this.contentType = this.contentType || "application/octet-stream";
     }
 
-    pipe(destination:Writable):Promise<Readable | ODataStream>{
+    pipe(destination: Writable): Promise<Readable | ODataStream> {
         return new Promise((resolve, reject) => {
             this.stream.on("open", () => {
-                if (typeof this.stream.close == "function"){
+                if (typeof this.stream.close == "function") {
                     destination.on("finish", () => {
                         this.stream.close();
                     });
@@ -30,10 +30,10 @@ export class ODataStream{
         });
     }
 
-    write(source:Readable):Promise<Writable | ODataStream>{
+    write(source: Readable): Promise<Writable | ODataStream> {
         return new Promise((resolve, reject) => {
             this.stream.on("open", () => {
-                if (typeof this.stream.close == "function"){
+                if (typeof this.stream.close == "function") {
                     source.on("finish", () => {
                         this.stream.close();
                     });
@@ -45,67 +45,69 @@ export class ODataStream{
     }
 }
 
-export interface IODataResult<T = {}>{
-    "@odata.context"?:string
-    "@odata.count"?:number
-    value?:T[]
-    [x: string]:any
+export interface IODataResult<T = {}> {
+    "@odata.context"?: string
+    "@odata.count"?: number
+    value?: T[]
+    [x: string]: any
 }
 
 export class ODataResult<T = {}>{
-    statusCode:number
-    body:IODataResult<T> & T
-    elementType:Function
-    contentType:string
-    stream?:any
+    statusCode: number
+    body: IODataResult<T> & T
+    elementType: Function
+    contentType: string
+    stream?: any
 
-    constructor(statusCode:number, contentType?:string, result?:any){
+    constructor(statusCode: number, contentType?: string, result?: any) {
         this.statusCode = statusCode;
-        if (typeof result != "undefined"){
+        if (typeof result != "undefined") {
             this.body = result;
             if (result && result.constructor) this.elementType = result.constructor;
             this.contentType = contentType || "application/json";
         }
     }
 
-    static Created = function Created(result:any, contentType?:string):Promise<ODataResult>{
-        if (result && typeof result.then == 'function'){
+    static Created = function Created(result: any, contentType?: string): Promise<ODataResult> {
+        if (result && typeof result.then == 'function') {
             return result.then((result) => {
                 return new ODataResult(201, contentType, result);
             });
-        }else{
+        } else {
             return Promise.resolve(new ODataResult(201, contentType, result));
         }
     }
 
-    static Ok = function Ok(result:any, contentType?:string):Promise<ODataResult>{
+    static Ok = function Ok(result: any, contentType?: string): Promise<ODataResult> {
         let inlinecount;
-        if (result && typeof result.then == 'function'){
+        if (result && typeof result.then == 'function') {
             return result.then((result) => {
-                if (result && Array.isArray(result)){
-                    if (result && (<any>result).inlinecount && typeof (<any>result).inlinecount == "number"){
+                if (result && Array.isArray(result)) {
+                    // if (result && (<any>result).inlinecount && typeof (<any>result).inlinecount == "number"){
+                    if (result && typeof (<any>result).inlinecount == "number") {
                         inlinecount = (<any>result).inlinecount;
                         delete (<any>result).inlinecount;
                     }
                     result = { value: result };
                     if (typeof inlinecount != "undefined") result["@odata.count"] = inlinecount;
-                }else{
-                    if (typeof result == "object" && result && typeof inlinecount == "number"){
+                } else {
+                    if (typeof result == "object" && result && typeof inlinecount == "number") {
                         result["@odata.count"] = inlinecount;
                     }
                 }
                 return new ODataResult(200, contentType, result);
             });
-        }else{
-            if (result && Array.isArray(result)){
-                if (result && (<any>result).inlinecount && typeof (<any>result).inlinecount == "number"){
+        } else {
+            if (result && Array.isArray(result)) {
+                // if (result && (<any>result).inlinecount && typeof (<any>result).inlinecount == "number"){
+                if (result && typeof (<any>result).inlinecount == "number") {
                     inlinecount = (<any>result).inlinecount;
                     delete (<any>result).inlinecount;
                 }
                 result = { value: result };
                 if (typeof inlinecount == "number") result["@odata.count"] = inlinecount;
-            }else{
-                if (typeof result == "object" && result && typeof inlinecount == "number"){
+            } else {
+                if (typeof result == "object" && result && typeof inlinecount == "number") {
                     result["@odata.count"] = inlinecount;
                 }
             }
@@ -113,10 +115,10 @@ export class ODataResult<T = {}>{
         }
     };
 
-    static NoContent = function NoContent(result?:any, contentType?:string):Promise<ODataResult>{
-        if (result && typeof result.then == 'function'){
+    static NoContent = function NoContent(result?: any, contentType?: string): Promise<ODataResult> {
+        if (result && typeof result.then == 'function') {
             return result.then(_ => new ODataResult(204, contentType));
-        }else{
+        } else {
             return Promise.resolve(new ODataResult(204, contentType));
         }
     }
